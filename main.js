9586642da98be351984b08972374aadb038b1ecc
Tobiaha <122260640+Tobiaha@users.noreply.github.com>
2025-09-01 22:39:31 +0300

    joo

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-function greeting() {
-  console.log("Hello World!");
-}
-greeting();
+'use strict';
+const dialog = document.querySelector('dialog');
+const showButton = document.querySelector('dialog + button');
+const closeButton = document.querySelector('dialog button');
+
+// "Show the dialog" button opens the dialog modally
+showButton.addEventListener('click', () => {
+  dialog.showModal();
+});
+
+// "Close" button closes the dialog
+closeButton.addEventListener('click', () => {
+  dialog.close();
+});
